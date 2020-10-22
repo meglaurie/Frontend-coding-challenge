34a4b9684bfbdb171f35354d32f0e38b28926565
meglaurie <megan.laurie@edu.sait.ca>
2020-10-22 16:30:09 -0600

    HTML to add image from comp to UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import data from './data.js';
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -7,3 +10,23 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+var date = new Date();
+
+console.log(date);
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#img')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
