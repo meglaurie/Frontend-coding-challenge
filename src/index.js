9c26f00fd6b5db3e8ee0bbeae0049ba4cdf4f221
meglaurie <megan.laurie@edu.sait.ca>
2020-10-26 17:47:57 -0600

    Image upload working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+// Mobile menu
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -9,34 +9,26 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
-
+// The Date
 var date = new Date();
-
 var day = date.getDate();
 var month = date.getMonth();
 var year = date.getFullYear();
 var monthsYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 for(var i = 0; i < monthsYear.length; i++){
   if(month == i){
     month = monthsYear[i];
   }
 }
-
-var uiDate =
 document.getElementById('date').innerHTML = month + " " + day + ", " + year;
 
-console.log(date);
-
-function readURL(input) {
+// Get image from user
+function uploadImg(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('#img')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);
+               document.getElementById("img").src = e.target.result;
             };
 
             reader.readAsDataURL(input.files[0]);
