7349841b2b900ea7d97dc99de7ff9f5172be6382
meglaurie <megan.laurie@edu.sait.ca>
2021-02-15 18:32:39 -0700

    can submit multiple times to UI

--- src/index.js
+++ src/index.js
@@ -9,53 +9,90 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
-// The Date
-var date = new Date();
-var day = date.getDate();
-var month = date.getMonth();
-var year = date.getFullYear();
-var monthsYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-for(var i = 0; i < monthsYear.length; i++){
-  if(month == i){
-    month = monthsYear[i];
-  }
-}
-document.getElementById('date').innerHTML = month + " " + day + ", " + year;
+const submit = document.getElementById('imgTxtSubmit');
+const img = document.getElementById('imgTxt-input');
+const cardContainer = document.getElementById('card-container');
+
+const card = document.createElement('div');
+card.classList.add("card");
+
+//Save to local storage
+// function saveLocalCards(card){
+//   let cards;
+//   if(localStorage.getItem('cards') === null){
+//     cards = [];
+//   }else{
+//     cards = JSON.parse(localStorage.getItem('cards'));
+//   }
+//   cards.push(card);
+//   localStorage.setItem('cards', JSON.stringify(cards));
+// }
+
 
 // Get image from user
 function uploadImg(input) {
-  console.log(input.files);
+  // console.log(input.files);
+  const cardImg = document.createElement('img');
+  
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-               document.getElementById("img").src = e.target.result;
-               console.log(this.result);
-               console.log(this);
+               cardImg.src = e.target.result;
+               cardImg.classList.add('card-img');
+               card.appendChild(cardImg);
             };
-            console.log(input.files[0]);
+            // console.log(input.files[0]);
             reader.readAsDataURL(input.files[0]);
         }
+        console.log('img upload');
     }
+
+
+// The Date
+
+function date(){
+  const cardDate = document.createElement('h4');
+  cardDate.classList.add('date');
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+  var monthsYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  for(var i = 0; i < monthsYear.length; i++){
+    if(month == i){
+      month = monthsYear[i];
+    }
+  }
+  cardDate.innerHTML = month + " " + day + ", " + year;
+  card.appendChild(cardDate);
+  console.log('Get date');
+};
 
 // Get text from user and clear input feild
 function getInputValue(){
-    var inputVal = document.getElementById("txt-input").value;
-    document.getElementById('card-txt').innerHTML = inputVal;
-    document.getElementById("txt-input").value = " ";
-
+    const cardTxt = document.createElement('p');
+    cardTxt.classList.add('card-txt');
+    var inputVal = document.getElementById("txtImg-input").value;
+    cardTxt.innerHTML = inputVal;
+    document.getElementById("txtImg-input").value = " ";
+    card.appendChild(cardTxt);
+    console.log('get text value');
 }
 
-console.log(document.getElementById('imgTxt-input').files);
-
-const submit = document.getElementById('imgTxtSubmit');
-const img = document.getElementById('imgTxt-input');
+function createCard(element){
+  const card = document.createElement('div');
+  card.classList.add("card");
+  card.appendChild(element);
+  cardContainer.appendChild(card);
+}
 
 function uploadImgTxt(){
-uploadImg(img);
-var inputVal = document.getElementById("txtImg-input").value;
-document.getElementById('card-txt').innerHTML = inputVal;
-document.getElementById('txtImg-input').value = " ";
- 
-  console.log("clicked");
+  uploadImg(img);
+  date()
+  getInputValue();
+  cardContainer.appendChild(card);
+  // if image has a value == true then createElement img 
+  // and appendchild to card appendChild to conatiner 
+// 
 }
