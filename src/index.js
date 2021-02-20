6cca18606caf27915d6e42f0fe707961f5a83230
meglaurie <megan.laurie@edu.sait.ca>
2021-02-19 17:58:43 -0700

    tinkering

--- src/index.js
+++ src/index.js
@@ -15,21 +15,98 @@ const submit = document.getElementById('imgTxtSubmit');
 const img = document.getElementById('imgTxt-input');
 const cardContainer = document.getElementById('card-container');
 
+
 const card = document.createElement('div');
 card.classList.add("card");
 
 
 // Save to local storage
 function saveLocalCards(card){
-  let cards;
-  if(localStorage.getItem('cards') === null){
-    cards = [];
+  let imgs;
+  let txt;
+  if(localStorage.getItem('imgs') === null){
+    imgs = [];
+    txt = [];
   }else{
-    cards = JSON.parse(localStorage.getItem('cards'));
+    imgs = JSON.parse(localStorage.getItem('imgs'));
   }
-  cards.push(card);
+  imgs.push(card);
+  console.log(card);
   localStorage.setItem('cards', JSON.stringify(cards));
 }
+
+function closeModal(){
+  modalContent.style.display = 'none';
+}
+
+function uploadImgTxt(){
+
+  const card = document.createElement('div');
+  card.classList.add("card");
+
+  // upload Image 
+  const cardImg = document.createElement('img');
+  cardImg.classList.add('card-img');
+        if (img.files && img.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+               cardImg.src = e.target.result;
+               card.appendChild(cardImg);
+            };
+            // console.log(input.files[0]);
+            reader.readAsDataURL(img.files[0]);
+        }
+  
+
+
+    // The date
+    const cardDate = document.createElement('h4');
+    cardDate.classList.add('date');
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var monthsYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    for(var i = 0; i < monthsYear.length; i++){
+      if(month == i){
+        month = monthsYear[i];
+      }
+    }
+    cardDate.innerHTML = month + " " + day + ", " + year;
+    card.appendChild(cardDate);
+   
+
+    // Get text 
+    const cardTxt = document.createElement('p');
+    cardTxt.classList.add('card-txt');
+    var inputVal = document.getElementById("txtImg-input").value;
+    cardTxt.innerHTML = inputVal;
+    document.getElementById("txtImg-input").value = " ";
+    card.appendChild(cardTxt);
+    const parent = document.getElementById('main');
+    parent.appendChild(cardContainer, card);
+    
+    closeModal();
+    // saveLocalCards(card);
+}
+
+let lastCard = uploadImgTxt();
+
+// localStorage.clear();
+
+displayBtn.addEventListener("click", function() {
+    modalContent.style.display = 'block';
+});
+
+const close = document.getElementById('modalClose');
+
+close.addEventListener("click", function(){
+  modalContent.style.display = 'none';
+});
+
+
+
 
 
 // Get image from user
@@ -91,85 +168,3 @@ function saveLocalCards(card){
 //   // and appendchild to card appendChild to conatiner 
 // // 
 // }
-
-function closeModal(){
-  modalContent.style.display = 'none';
-  console.log('close');
-}
-
-function uploadImgTxt(){
-
-  const card = document.createElement('div');
-  card.classList.add("card");
-
-  // upload Image 
-  const cardImg = document.createElement('img');
-  cardImg.classList.add('card-img');
-        if (img.files && img.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-               cardImg.src = e.target.result;
-               card.appendChild(cardImg);
-            };
-            // console.log(input.files[0]);
-            reader.readAsDataURL(img.files[0]);
-        }
-        console.log('img upload');
-
-
-    // The date
-    const cardDate = document.createElement('h4');
-    cardDate.classList.add('date');
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-    var monthsYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    for(var i = 0; i < monthsYear.length; i++){
-      if(month == i){
-        month = monthsYear[i];
-      }
-    }
-    cardDate.innerHTML = month + " " + day + ", " + year;
-    card.appendChild(cardDate);
-    console.log('Get date');
-
-    // Get text 
-    const cardTxt = document.createElement('p');
-    cardTxt.classList.add('card-txt');
-    var inputVal = document.getElementById("txtImg-input").value;
-    cardTxt.innerHTML = inputVal;
-    document.getElementById("txtImg-input").value = " ";
-    card.appendChild(cardTxt);
-    console.log('get text value');
-
-    cardContainer.appendChild(card);
-
-    closeModal();
-  
-    // let cards;
-    // if(localStorage.getItem('cards') === null){
-    //   cards = [];
-    // }else{
-    //   cards = JSON.parse(localStorage.getItem('cards'));
-    // }
-    // cards.push(card);
-    // localStorage.setItem('cards', JSON.stringify(cards));
-}
-
-// localStorage.clear();
-
-displayBtn.addEventListener("click", function() {
-  if(modalContent.style.display == 'none'){
-    modalContent.style.display = 'block';
-  }else{
-    modalContent.style.display = 'none';
-  }
-});
-
-const close = document.getElementById('modalClose');
-
-close.addEventListener("click", function(){
-  modalContent.style.display = 'none';
-});
